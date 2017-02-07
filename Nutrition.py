8c3b5f89ca8e9f6e14bed19e6538276061db97d1
Sharif Shameem <samin100@gmail.com>
2017-02-07 18:28:53 -0500

    Add files via upload

---
+++ Nutrition.py
@@ -0,0 +1,29 @@
+import myfitnesspal
+
+client = myfitnesspal.Client('samin100')
+day = client.get_date(2016, 12, 28)  # (year, month, day) format
+
+
+# an example of accessing nutrition data with the API
+
+print('Meals I ate on ' + str(day.date) + '\n')
+
+mealTypes = ['Breakfast', 'Lunch', 'Dinner', 'Snacks']
+typeCount = 0
+dayTotal = 0
+
+
+for meal in day.meals:
+
+    mealTotal = 0
+    print(mealTypes[typeCount] + ': ')
+    for entry in meal.entries:
+        print(entry.name)  # entry.name gives us only the name of the food as a string, not it's nutritional data
+        mealTotal += entry['calories']
+
+    print('Meal total: ' + str(mealTotal) + ' calories\n')
+    dayTotal += mealTotal
+    typeCount += 1
+
+
+print('Daily today: ' + str(dayTotal) + ' calories')

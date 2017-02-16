cd5deb56adc298a8fe38df28e4693d9c9a58dcff
Sharif Shameem <samin100@gmail.com>
2017-02-15 20:57:26 -0500

    Switching from Django to Flask

--- mysite/NutritionAnalyzer/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url, include
-from . import views  # imports the local views.py file
-
-urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    ]  # if the url is regex ^$ then we're going to return the views.index
--- mysite/mysite/secrets.py
+++
@@ -1,2 +0,0 @@
-#  used to store sensitive information that won't get pushed to GitHub
-SECRET_KEY = 'rw$z2^9=hl&$+d1a4%gje506b5hj6+9j#@%9pq51g0$10oavop'
